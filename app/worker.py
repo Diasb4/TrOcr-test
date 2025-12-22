7c77acca03c45a92f35c8029fd1202ed9d6f10ed
Dias Tursynbai <133123959+Diasb4@users.noreply.github.com>
2025-12-22 10:04:46 +0500

    Reapply "Update response header and JSON output keys for clarity in processing time"
    This reverts commit ae5483331c554e98b615d064f189c17fbbe4ee4a.

--- app/worker.py
+++ app/worker.py
@@ -20,8 +20,9 @@ def process_pdf_job(job_id: str, pdf_path: str, phrase: str):
         json.dump(
             {
                 "job_id": job_id,
-                "processing_time_ms": round(duration, 2),
-                "result": result
+                "result": result,
+                # "pdf_processing_time_ms": round(pdf_duration, 2),
+                "total_processing_time_ms": round(total_duration, 2)
             },
             f,
             ensure_ascii=False,
